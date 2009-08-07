81aeab223693d1388b49464a1b19d0036c326a93
Edward Lee <edilee@mozilla.com>
2009-08-07 16:26:13 -0700

    Rewrite override stuff to just override autocomplete search and suggest emails based on name.
    --HG--
    extra : rebase_source : 8c27c5f499d83f0fe063884792def075f6d04072

--- components/nsFormFillOverride.js
+++ components/nsFormFillOverride.js
@@ -2,84 +2,76 @@ const Cc = Components.classes;
 const Ci = Components.interfaces;
 const Cu = Components.utils;
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://people/modules/people.js");
 
 function _() {
   let msg = Array.join(arguments, " ");
   dump(msg + "\n");
   Cu.reportError(msg);
 }
+_("loaded");
 
-var originalImpls = {
-  _mainFFImpl: null,
-  get mainFFImpl() {
-    if (!this._mainFFImpl)
-      this._mainFFImpl = Components.classesByID[
-        Components.ID('{895DB6C7-DBDF-40ea-9F64-B175033243DC}')
-      ].getService(Ci.nsIFormFillController);
-    return this._mainFFImpl;
-  },
-  
-  _mainACImpl: null,
-  get mainACImpl() {
-    if (!this._mainACImpl)
-      this._mainACImpl = Components.classesByID[
-        Components.ID('{895DB6C7-DBDF-40ea-9F64-B175033243DC}')
-      ].getService(Ci.nsIAutoCompleteSearch);
-    return this._mainACImpl;
-  }
+__defineGetter__("FACS", function() {
+  _("get FACS");
+  delete this.FACS;
+  return this.FACS = Components.classesByID["{895db6c7-dbdf-40ea-9f64-b175033243dc}"].
+    getService(Ci.nsIAutoCompleteSearch);
+});
+function PeopleAutoCompleteSearch() {
+  _("new PACS");
 }
-
-function FormHistoryOverride() {
-}
-FormHistoryOverride.prototype = {
-  // XPCOM registration
-  classDescription: "Override",
-  contractID:       "@mozilla.org/satchel/form-fill-controller;1",
-  classID:          Components.ID("{106471bb-b86a-4659-9d37-dca54019f6e2}"),
-
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIFormFillController,
-                                         Ci.nsIObserver,
-                                         Ci.nsISupportsWeakReference,
-                                         Ci.nsIAutoCompleteInput])
-}
-FormHistoryOverride.prototype.__proto__ = originalImpls.mainFFImpl;
-
-
-function FormAutoCompleteOverride() {
-}
-FormAutoCompleteOverride.prototype = {
-  // XPCOM registration
-  classDescription: "Override FH AC",
+PeopleAutoCompleteSearch.prototype = {
+  classDescription: "People AutoComplete Search",
   contractID:       "@mozilla.org/autocomplete/search;1?name=form-history",
   classID:          Components.ID("{9ed95942-f031-436c-8540-8d5e222c6fe2}"),
+  QueryInterface: XPCOMUtils.generateQI([
+    Ci.nsIAutoCompleteSearch,
+    Ci.nsIAutoCompleteSimpleResultListener
+  ]),
 
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIAutoCompleteSearch]),
-
-  _isEmailSearch: function(param) {
-    return true;
+  checkPeople: function checkPeople(param) {
+    return param.search(/e-?mail/) != -1;
   },
 
-  startSearch: function (searchString, searchParam, prevResult, listener) {
-    _("startSearch called");
-    if (this._isEmailSearch(searchParam)) {
-      _("doing email search: " + searchString);
-      var input = Cc["@mozilla.org/satchel/form-fill-controller;1"];
-      var FAC = Cc["@mozilla.org/satchel/form-autocomplete;1"].
-                getService(Ci.nsIFormAutoComplete);
-      var result = FAC.autoCompleteSearch(searchParam, searchString,
-                   originalImpls.mainFFImpl.input, prevResult);
-      listener.onSearchResult(this, result);
-    } else {
-      originalImpls.mainACImpl.startSearch(searchString, searchParam, 
-        prevResult, listener);
-    }
+  findPeople: function findPeople(string, listener) {
+    let result = Cc["@mozilla.org/autocomplete/simple-result;1"].
+      createInstance(Ci.nsIAutoCompleteSimpleResult);
+    result.setSearchString(string);
+    result.setListener(this);
+
+    // Match the name and show the email for now..
+    People.find({ displayName: string }).forEach(function(person) {
+      result.appendMatch(person.documents.default.emails[0].value, "");
+    });
+
+    let resultCode = result.matchCount ? "RESULT_SUCCESS" : "RESULT_NOMATCH";
+    result.setSearchResult(Ci.nsIAutoCompleteResult[resultCode]);
+
+    listener.onSearchResult(this, result);
   },
-  
+
+  startSearch: function(string, param, prev, listener) {
+    _("start search", Array.slice(arguments));
+
+    // Do people searches for certain queries
+    if (this.checkPeople(param))
+      return this.findPeople(string, listener);
+
+    // Use the base form search for non-people searches
+    let PACS = this;
+    FACS.startSearch(string, param, prev, {
+      onSearchResult: function(search, result) {
+        _("on search result");
+        listener.onSearchResult(PACS, result);
+      }
+    });
+  },
+
   stopSearch: function() {
-    originalImpls.mainACImpl.stopSearch();
+    _("stop search", Array.slice(arguments));
+    FACS.stopSearch();
   }
-}
+};
 
-var components = [FormHistoryOverride, FormAutoCompleteOverride];
-function NSGetModule(compMgr, fileSpec)
-  XPCOMUtils.generateModule(components);
+let components = [PeopleAutoCompleteSearch];
+function NSGetModule(compMgr, fileSpec) XPCOMUtils.generateModule(components);
