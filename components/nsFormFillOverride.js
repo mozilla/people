bd85ee25d86ad682b9e47d13fe7f802898432502
Anant Narayanan <anant@kix.in>
2009-08-07 11:35:35 -0700

    Make formfilloverride semi-work
    --HG--
    extra : rebase_source : 5bd4dcd40ab67bc2ce41bb1de29fa352c3163e65

--- components/nsFormFillOverride.js
+++ components/nsFormFillOverride.js
@@ -1,12 +1,30 @@
 const Cc = Components.classes;
 const Ci = Components.interfaces;
+const Cu = Components.utils;
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
-Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
-
-function FormHistoryOverride() {
+var originalImpls = {
+  _mainFFImpl: null,
+  get mainFFImpl() {
+    if (!this._mainFFImpl)
+      this._mainFFImpl = Components.classesByID[
+        Components.ID('{895DB6C7-DBDF-40ea-9F64-B175033243DC}')
+      ].getService(Ci.nsIFormFillController);
+    return this._mainFFImpl;
+  },
   
+  _mainACImpl: null,
+  get mainACImpl() {
+    if (!this._mainACImpl)
+      this._mainACImpl = Components.classesByID[
+        Components.ID('{895DB6C7-DBDF-40ea-9F64-B175033243DC}')
+      ].getService(Ci.nsIAutoCompleteSearch);
+    return this._mainACImpl;
+  }
 }
 
+function FormHistoryOverride() {
+}
 FormHistoryOverride.prototype = {
   // XPCOM registration
   classDescription: "Override",
@@ -15,45 +33,14 @@ FormHistoryOverride.prototype = {
 
   QueryInterface: XPCOMUtils.generateQI([Ci.nsIFormFillController,
                                          Ci.nsIObserver,
-                                         Ci.nsISupportsWeakReference]),
-
-  attachToBrowser: function (docShell, popup) {
-    dump("attachToBrowser called\n");
-    originalImpls.mainFFImpl.attachToBrowser(docShell, popup);
-  },
-  
-  detachFromBrowser: function (docShell) {
-    originalImpls.mainFFImpl.detachFromBrowser(docShell);
-  },
-   
-  markAsLoginManagerField: function (input) {
-    originalImpls.mainFFImpl.markAsLoginManagerField(input);
-  }
+                                         Ci.nsISupportsWeakReference,
+                                         Ci.nsIAutoCompleteInput])
 }
+FormHistoryOverride.prototype.__proto__ = originalImpls.mainFFImpl;
 
-var originalImpls = {
-  _mainFFImpl: null,
-  get mainFFImpl() {
-    if (!this._mainFFImpl)
-      this._mainFFImpl = Components.classesByID[Components.ID('{895DB6C7-DBDF-40ea-9F64-B175033243DC}')]
-                                    .getService(Ci.nsIFormFillController);
-    return this._mainFFImpl;
-  },
-  
-  _mainACImpl: null,
-  get mainACImpl() {
-    if (!this._mainACImpl)
-      this._mainACImpl = Components.classesByID[Components.ID('{895DB6C7-DBDF-40ea-9F64-B175033243DC}')]
-                                    .getService(Ci.nsIAutoCompleteSearch);
-
-    return this._mainACImpl;
-  }
-}
 
 function FormAutoCompleteOverride() {
-
 }
-
 FormAutoCompleteOverride.prototype = {
   // XPCOM registration
   classDescription: "Override FH AC",
@@ -61,7 +48,6 @@ FormAutoCompleteOverride.prototype = {
   classID:          Components.ID("{9ed95942-f031-436c-8540-8d5e222c6fe2}"),
 
   QueryInterface: XPCOMUtils.generateQI([Ci.nsIAutoCompleteSearch]),
-
 
   _isEmailSearch: function(param) {
     return false;
@@ -71,22 +57,20 @@ FormAutoCompleteOverride.prototype = {
     dump("startSearch called\n");
     if (this._isEmailSearch(searchParam)) {
       dump("doing email search: " + searchString + "\n");
-      var input = Cc["@mozilla.org/satchel/form-fill-controller;1"]
-      var FAC = Components.classes["@mozilla.org/satchel/form-autocomplete;1"].getService(Ci.nsIFormAutoComplete);
-      var result = FAC.autoCompleteSearch(searchParam, searchString, originalImpls.mainFFImpl.input, prevResult);
+      var input = Cc["@mozilla.org/satchel/form-fill-controller;1"];
+      var FAC = Cc["@mozilla.org/satchel/form-autocomplete;1"].
+                getService(Ci.nsIFormAutoComplete);
+      var result = FAC.autoCompleteSearch(searchParam, searchString,
+                   originalImpls.mainFFImpl.input, prevResult);
       listener.onSearchResult(this, result);
+    } else {
+      originalImpls.mainACImpl.startSearch(searchString, searchParam, 
+        prevResult, listener);
     }
-    else
-      originalImpls.mainACImpl.startSearch(searchString, searchParam, prevResult, listener);
-  },
-  
-  stopSearch: function () {
-    originalImpls.mainACImpl.stopSearch();
-  },
-
+  }
 }
+FormAutoCompleteOverride.prototype.__proto__ = originalImpls.mainACImpl;
 
 var components = [FormHistoryOverride, FormAutoCompleteOverride];
-
 function NSGetModule(compMgr, fileSpec)
   XPCOMUtils.generateModule(components);
